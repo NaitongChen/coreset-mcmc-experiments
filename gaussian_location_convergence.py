e55e9c04b1b2c0ae3c91fd34197c229b63c2c6f3
Naitong Chen <35083192+NaitongChen@users.noreply.github.com>
2024-03-08 20:09:42 -0800

    Add files via upload

--- gaussian_location_convergence.py
+++ gaussian_location_convergence.py
@@ -18,15 +18,8 @@ def kl_heuristic(x, N, M, t, S, K, d):
         mean = N/(2*M)*np.exp(-2*c*np.log(t))
     else:
         mean = N/(2*M)*np.exp(-2*c*(t**alpha-1.)/alpha)
-    #var = gamma**2*(N-S)*M**2*(K+d)*d/(2*S*(K-1))*var_integral(gamma,alpha,M,t)
     var = c*(N-S)*(K+d)*d/(4*S*(K-1))*(1+(1-alpha)*np.log(t))/t**(1-alpha)
     return mean+var
-
-#def var_integral(gamma, alpha, M, t):
-#    if alpha < 1e-8:
-#        return quad( lambda tau : tau**(2*(alpha-1))*np.exp(-2*gamma*M*(np.log(t) - np.log(tau))), 1.0, t, limit=200, epsabs=1e-8, epsrel=1e-8)[0]
-#    else:
-#        return quad( lambda tau : tau**(2*(alpha-1))*np.exp(-2*gamma*M*(t**alpha-tau**alpha)/alpha), 1.0, t, limit=200, epsabs=1e-8, epsrel=1e-8)[0]
 
 def tune_gamma_alpha(N, M, S, K, d, T):
     alphas = np.linspace(0, 1, 500)
@@ -54,19 +47,6 @@ def tune_gamma(N, M, S, K, d, T, alpha):
             klmin = kl
             gammamin = gammas[j]
     return gammamin*N/M
-
-
-#def tune_gamma_alpha(N, M, S, K, d, T):
-#    c0 = 1.
-#    alpha0 = 0.5
-#    x0 = np.array([c0, alpha0])
-#    res = minimize(lambda x : kl_heuristic(x, N, M, T, S, K, d), x0, bounds = [(0., 0.1), (0., 1.)], tol=1e-12)
-#    return res.x[0]*N/M, res.x[1]
-
-#def tune_gamma(N, M, S, K, d, T, alpha):
-#    x0 = np.array([1.])
-#    res = minimize(lambda x : kl_heuristic(np.array([x[0], alpha]), N, M, T, S, K, d), x0, bounds = [(0., 0.1)], tol=1e-12)
-#    return res.x*N/M
 
 def gamma_t(t, alpha, gamma):
     return gamma*(t+1.)**(alpha-1.)
@@ -110,119 +90,12 @@ def est_dkl(w, Y, X, Z, S, replace):
     g2 = dZ.T.dot(dy) - (w.sum()-N)/2*dR
     return (g1*g2).sum(axis=1)/(K-1)
 
-#def expected_kl(ts, w0, Y, X, S, K, gamma):
-#    d, N = X.shape
-#    M = Y.shape[1]
-#    gammas = gamma(ts)
-#    kl0 = (((Y*w0).sum(axis=1))**2).sum()/(2*(N+1))
-#    cgammas = np.hstack((0, np.cumsum(gammas)))[:-1]
-#    bias_t = np.exp(-2*M/(N+1)*cgammas)*kl0
-#    noise_t = M**2*N**2*(K+d)*d*(N-S)/(2*S*(K-1)*(N-1)*(N+1)**3) * (gammas**2*np.exp(-2*M/(N+1)*cgammas)).sum() # NOTE: the last term here is slightly wrong due to indexing (off by one). shouldn't affect asymptotics
-#    return bias_t + noise_t
-
-
 np.random.seed(0)
 d = 20 # dimension
 N = 10000 # number of data pts
 X = np.random.randn(d, N) # dataset
 T = 100000 # total number of iterations
 L = 1 # number of trials
-
-## varying S test
-#if False:
-#    # Coreset MCMC, KL vs work done & KL vs iteration, varying S
-#    # test with/without replace
-#    alpha = 0.5 # learning rate power
-#    beta = 0.5 # uniform ergodicity constant (0 is iid, 1 is no mixing)
-#    K = d
-#    M = 30 # number of coreset pts
-#    Y = X[:, :M] # uniformly random coreset pts (because X is drawn iid, can just take first M)
-#    replace = False
-#    project = False
-#    S = np.array([1, 10, 100, 1000, 10000])
-#    fn = f"vary_S_replace_{replace}_beta_{beta}_alpha_{alpha}.csv"
-#    if os.path.exists(fn):
-#        df = pd.read_csv(fn)
-#    else:
-#        df = pd.DataFrame(columns=["trial", "subsample_size", "iteration", "work", "KL"])
-#    for ell in range(L):
-#        print(f"Trial {ell+1}/{L}")
-#        for i in range(S.shape[0]):
-#            if df[ (df["subsample_size"] == S[i]) & (df["trial"] == ell) ].empty:
-#                np.random.seed(ell*S.shape[0]+i)
-#                print(f"S = {S[i]}, i = {i+1}/{S.shape[0]}")
-#                # initialize at even weighting
-#                w = (N/M)*np.ones(M)
-#                Z = draw_w(K, w, Y)
-#                C = 0.01*N/M *(M+S[i])**alpha # learning rate constant
-#                Ws = np.zeros(T)
-#                KLs = np.zeros(T)
-#                sumws = np.zeros(T)
-#                KLs[0] = kl(w,Y,X)
-#                sumws[0] = w.sum()
-#                for t in range(1, T):
-#                    if t % 100 == 0:
-#                        sys.stdout.write(f"t = {t+1}/{T}              \r")
-#                        sys.stdout.flush()
-#                    # projected gradient estimate
-#                    Z = markov_step(beta, Z, w, Y)
-#                    g = est_dkl(w, Y, X, Z, S[i], replace=replace)
-#                    if project:
-#                        g -= g.sum()/g.shape[0]*np.ones(g.shape[0])
-#                    w -= gamma(t, alpha, C)*g
-#                    # record KL / work done
-#                    KLs[t] = kl(w,Y,X)
-#                    sumws[t] = w.sum()
-#                    Ws[t] = Ws[t-1] + (M+S[i])*d
-#                # write to df and save
-#                dfi = pd.DataFrame({"trial": ell*np.ones(T),
-#                                    "subsample_size": S[i]*np.ones(T),
-#                                    "iteration": np.arange(T),
-#                                    "work": Ws,
-#                                    "KL": KLs,
-#                                    "sumw": sumws
-#                                })
-#                df = pd.concat((df, dfi), ignore_index=True)
-#                df.to_csv(fn, index=False)
-#            else:
-#                print(f"Already ran S = {S[i]}, i = {i+1}/{S.shape[0]}")
-#            print("")
-#
-#    # plot the results
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111)
-#    for i in range(S.shape[0]):
-#        # plot individual traces
-#        subdf = df[(df["subsample_size"] == S[i]) & (df["trial"] == 0)]
-#        line, = ax.plot(subdf["work"], subdf["KL"], label=f"S = {S[i]}")
-#        for ell in range(1, L):
-#            subdf = df[(df["subsample_size"] == S[i]) & (df["trial"] == ell)]
-#            ax.plot(subdf["work"], subdf["KL"], color=line.get_color())
-#    ax.set_yscale('log')
-#    ax.set_xscale('log')
-#    ax.set_title(fn)
-#    ax.set_ylabel('KL Divergence')
-#    ax.set_xlabel('Cost')
-#    ax.legend()
-#    plt.show()
-#
-#    # plot the results
-#    fig = plt.figure()
-#    ax = fig.add_subplot(111)
-#    for i in range(S.shape[0]):
-#        # plot individual traces
-#        subdf = df[(df["subsample_size"] == S[i]) & (df["trial"] == 0)]
-#        line, = ax.plot(subdf["work"], subdf["sumw"], label=f"S = {S[i]}")
-#        for ell in range(1, L):
-#            subdf = df[(df["subsample_size"] == S[i]) & (df["trial"] == ell)]
-#            ax.plot(subdf["work"], subdf["sumw"], color=line.get_color())
-#    ax.set_yscale('log')
-#    ax.set_xscale('log')
-#    ax.set_title(fn)
-#    ax.set_ylabel('Sum of Weights')
-#    ax.set_xlabel('Cost')
-#    ax.legend()
-#    plt.show()
 
 # varying K test
 if False:
@@ -245,9 +118,6 @@ if False:
                 np.random.seed(ell*Ks.shape[0]+i)
                 print(f"K = {Ks[i]}, i = {i+1}/{Ks.shape[0]}")
                 # initialize at even weighting
-                #gamma, alpha = tune_gamma_alpha(N, M, S, Ks[i], d, 10000)
-                #alpha = 0.5
-                #gamma = tune_gamma(N, M, S, Ks[i], d, 10000, alpha)
                 gamma = 0.05*N/M
                 alpha = 1.0
                 gamma_max = 0.1*N/M
@@ -302,7 +172,6 @@ if False:
             ax.plot(subdf["work"], subdf["KL"], color=line.get_color())
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('KL Divergence')
     ax.set_xlabel('Cost')
     ax.legend()
@@ -320,7 +189,6 @@ if False:
             ax.plot(subdf["iteration"], subdf["sumw"], color=line.get_color())
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Sum of Weights')
     ax.set_xlabel('Iteration')
     ax.legend()
@@ -336,7 +204,6 @@ if False:
         line, = ax.plot(Ks, gammas)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Gamma')
     ax.set_xlabel('K')
     ax.legend()
@@ -352,7 +219,6 @@ if False:
         line, = ax.plot(Ks, alphas)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Alpha')
     ax.set_xlabel('K')
     ax.legend()
@@ -362,7 +228,6 @@ if False:
 if False:
     # Coreset MCMC, KL vs work done & KL vs iteration, varying K
     # test with/without replace
-    #alpha = 0.5 # learning rate power
     beta = 0.8 # uniform ergodicity constant (0 is iid, 1 is no mixing)
     K = d # number of coreset pts
     replace = False
@@ -383,15 +248,10 @@ if False:
                 # initialize at even weighting
                 M = Ms[i]
                 Y = X[:, :M] # uniformly random coreset pts (because X is drawn iid, can just take first M)
-                #C = 0.1*N/M #* (M+S)**alpha # learning rate constant
                 # initialize at even weighting
-                #gamma, alpha = tune_gamma_alpha(N, Ms[i], S, K, d, 10000)
                 gamma = 0.1*N/M
                 alpha = 0.5
                 print(f"Tuned gamma, alpha: {gamma}, {alpha}")
-                #alpha = 0.5
-                #gamma = tune_gamma(N, Ms[i], S, K, d, 10000, alpha)
-                #print(f"Tuned gamma, alpha=0.5: {gamma}")
                 w = (N/M)*np.ones(M)
                 Z = draw_w(Ms[i], w, Y)
                 Ws = np.zeros(T)
@@ -441,7 +301,6 @@ if False:
             ax.plot(subdf["work"], subdf["KL"], color=line.get_color())
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('KL Divergence')
     ax.set_xlabel('Cost')
     ax.legend(loc='upper right', fontsize=12)
@@ -475,7 +334,6 @@ if False:
         line, = ax.plot(Ms, gammas)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Gamma')
     ax.set_xlabel('M')
     ax.legend()
@@ -491,7 +349,6 @@ if False:
         line, = ax.plot(Ms, alphas)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Alpha')
     ax.set_xlabel('M')
     ax.legend()
@@ -501,7 +358,7 @@ if False:
 if True:
     # Coreset MCMC, KL vs work done & KL vs iteration, varying K
     # test with/without replace
-    #alpha = 0.5 # learning rate power
+    # alpha = 0.5 # learning rate power
     K = d # number of coreset pts
     replace = False
     project = True
@@ -579,7 +436,6 @@ if True:
             ax.plot(subdf["work"], subdf["KL"], color=line.get_color())
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('KL Divergence')
     ax.set_xlabel('Cost')
     ax.legend(loc='lower left', fontsize=12)
@@ -597,7 +453,6 @@ if True:
             ax.plot(subdf["iteration"], subdf["sumw"], color=line.get_color())
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Sum of Weights')
     ax.set_xlabel('Iteration')
     ax.legend(loc='upper right', fontsize=12)
@@ -613,7 +468,6 @@ if True:
         line, = ax.plot(betas, gammas)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Gamma')
     ax.set_xlabel('Beta')
     ax.legend()
@@ -629,66 +483,7 @@ if True:
         line, = ax.plot(betas, alphas)
     ax.set_yscale('log')
     ax.set_xscale('log')
-    #ax.set_title(fn)
     ax.set_ylabel('Alpha')
     ax.set_xlabel('Beta')
     ax.legend()
     plt.show()
-
-
-
-# test 3: Coreset MCMC, KL vs work done & KL vs iteration, factor: K (with replacement)
-# test 4: Coreset MCMC, KL vs work done & KL vs iteration, factor: K (without replacement)
-# test 5:
-
-
-
-
-#K = 5 # number of Markov chains
-#Z = draw_w(K, w, Y)
-#
-#
-#
-#ts = np.arange(10000)
-#S = np.sqrt(N)
-#kls = expected_kl(ts, w, Y, X, S, K, lambda t : N/M*(1/np.sqrt(N))*(1./np.sqrt(1+t)))
-#
-#plt.plot(ts, kls)
-#plt.yscale('log')
-#plt.show()
-#quit()
-
-
-
-
-# # test full/stochastic gradient (these tests pass)
-# g = dkl(w,Y,X)
-# gn = np.zeros(g.shape)
-# for i in range(g.shape[0]):
-#     wi = w.copy()
-#     wi[i] += 1e-4
-#     gn[i] = (kl(wi, Y, X) - kl(w, Y, X))/1e-4
-# print(g)
-# print(gn)
-# gh = np.zeros(g.shape)
-# B = 100000
-# for i in range(B):
-#     gh += est_dkl(w,Y,X,Z)
-#     Z = markov_step(0.0, Z, w, Y)
-# print(g)
-# print(gh/B)
-# quit()
-
-#
-## full gradient descent
-#for i in range(1000000):
-#    print(f"Current KL: {kl(w,Y,X)} Current wsum: {w.sum()}")
-#    w = w - 50*dkl(w,Y,X)
-#quit()
-#
-## stochastic gradient descent
-#for i in range(1000):
-#    print(f"Current KL: {kl(w,Y,X)} Current wsum: {w.sum()}")
-#
-
-
